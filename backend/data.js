f25908b32798e7f2b426a339b0e32ce7bbe05ee2
KANISHGA <168720129+kanishgask@users.noreply.github.com>
2026-05-03 08:50:46 +0530

    Create data.js

---
+++ backend/data.js
@@ -0,0 +1,11 @@
+const courses = [
+    { id: 1, topic: "DSA", level: "beginner", title: "Arrays Basics" },
+    { id: 2, topic: "DSA", level: "intermediate", title: "Linked List Problems" },
+    { id: 3, topic: "DSA", level: "advanced", title: "Dynamic Programming" },
+
+    { id: 4, topic: "Web", level: "beginner", title: "HTML & CSS" },
+    { id: 5, topic: "Web", level: "intermediate", title: "React Basics" },
+    { id: 6, topic: "Web", level: "advanced", title: "Full Stack Development" }
+];
+
+module.exports = { courses };
