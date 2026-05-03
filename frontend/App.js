283cadc27e5e02fcd897c3482c0d717666baea88
KANISHGA <168720129+kanishgask@users.noreply.github.com>
2026-05-03 08:50:23 +0530

    Create App.js

---
+++ frontend/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LearningForm from "./components/LearningForm";
+
+function App() {
+  return (
+    <div>
+      <h1>AI Personalized Learning System</h1>
+      <LearningForm />
+    </div>
+  );
+}
+
+export default App;
